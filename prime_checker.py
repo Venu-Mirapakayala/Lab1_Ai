a843da63320c85d8f13dfb2bfb180ab85c668d7c
Venu Mirapakayala <venuroyal0003@gmail.com>
2025-01-26 17:50:51 -0500

    Add prime number checker script

---
+++ prime_checker.py
@@ -0,0 +1,16 @@
+# Prime Number Checker
+
+def is_prime(number):
+    if number <= 1:
+        return False
+    for i in range(2, int(number**0.5) + 1):
+        if number % i == 0:
+            return False
+    return True
+
+if __name__ == "__main__":
+    num = int(input("Enter a number to check if it's prime: "))
+    if is_prime(num):
+        print(f"{num} is a prime number.")
+    else:
+        print(f"{num} is not a prime number.")
